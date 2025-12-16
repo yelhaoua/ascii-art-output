0519b9e07696a087130c0118b67fabd83a57e6c3
hchaitbenal <hichameaitbenalla@gmail.com>
2025-12-16 10:34:04 +0100

    add: Print fun and fix the main to handel all options to run

--- func/printsymbole.go
+++ func/printsymbole.go
@@ -1,5 +1,52 @@
 package asciiart
 
-func PrintSymbole(arr [][]string, woord string) {
+import (
+	"fmt"
+	"strings"
+)
+
+// func SaveFile(text , name string){
+
+// }
+
+func Output(str string, array [][]string) {
+	// out := ""
+	// start looping to8 the hieght of line
+	for i := 0; i < 8; i++ {
+		for _, fin := range str {
+			// skip the unprintibal chachacters
+			if fin > 126 || fin < 32 {
+				continue
+			}
+			// out += array[int(rune(fin)-32)][i]
+			fmt.Print(array[int(rune(fin)-32)][i])
+		}
+		// out += "\n"
+		fmt.Println()
+	}
+	// SaveFile(out, name)
+	// out = ""
+}
+
+func PrintSymbole(array [][]string, word string) {
+	// check if the word not empty and the array of banner
+	if word == "" || len(array) == 0 {
+		return
+	}
+	// change the \n as string to newline
+	str := strings.ReplaceAll(word, `\n`, "\n")
+
+	// skip addional newline in print
+	if strings.Trim(str, "\n") == "" {
+		fmt.Print(str)
+		return
+	}
+	// spliting the word
+	words := strings.Split(word, "\n")
+
+	// Print each word 
+	for _, s := range words {
+		Output(s, array)
+	}
 
 }
--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	asciiart "asciiart/func"
+	"fmt"
 	"os"
+
+	asciiart "asciiart/func"
 )
 
 func main() {
-	if len(os.Args) != 3 {
+	// check the num of args
+	if len(os.Args) > 4 || len(os.Args) == 1 {
 		return
-	} else {
-		file := asciiart.Splite(os.Args[1])
-		asciiart.PrintSymbole(file, os.Args[2])
 	}
-
+	// add the logic for run program in all cases just temp
+	switch len(os.Args) {
+	case 2:
+		Content := asciiart.Splite("standard")
+		asciiart.PrintSymbole(Content, os.Args[1])
+	case 3:
+		Content := asciiart.Splite(os.Args[2])
+		asciiart.PrintSymbole(Content, os.Args[1])
+	case 4:
+		fmt.Println(os.Args[1][0:8])
+		if os.Args[1][0:9] != "--output=" {
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]")
+			return
+		}
+		// Split the banner (os.Args[2] == name of banner)
+		Content := asciiart.Splite(os.Args[3])
+		// Print the text of user input  (os.Args[1] == text)
+		asciiart.PrintSymbole(Content, os.Args[2])
+	}
 }
