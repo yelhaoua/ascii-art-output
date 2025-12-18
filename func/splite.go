1a10f554e3f648e33bb9e780c837525c33b7ca6c
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-18 12:48:24 +0100

    adding comment explane the code

--- func/splite.go
+++ func/splite.go
@@ -8,11 +8,17 @@ import (
 func Splite(fileName string) [][]string {
 	var symbole []string
 	var allData [][]string
+	
+	// read the bannere from banners folder
 	data, err := os.ReadFile("./banners/" + fileName + ".txt")
 	if err != nil {
 		return [][]string{}
 	}
+
+	// splite the content of bannere whit "\n"
 	symbole = strings.Split(string(data), "\n")
+
+	// add each symbole the an array
 	for i := 1; i < len(symbole); i += 9 {
 		if i+8 < len(symbole) {
 			allData = append(allData, symbole[i:i+8])
