66559ae9100d47f8c93dc07e45948c09d0e32a6d
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-18 11:02:56 +0100

    adding splite function

--- func/splite.go
+++ func/splite.go
@@ -1,5 +1,22 @@
 package asciiart
 
+import (
+	"os"
+	"strings"
+)
+
 func Splite(fileName string) [][]string {
-	return [][]string{}
+	var symbole []string
+	var allData [][]string
+	data, err := os.ReadFile("./banners/" + fileName + ".txt")
+	if err != nil {
+		return [][]string{}
+	}
+	symbole = strings.Split(string(data), "\n")
+	for i := 1; i < len(symbole); i += 9 {
+		if i+8 < len(symbole) {
+			allData = append(allData, symbole[i:i+8])
+		}
+	}
+	return allData
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	asciiart "asciiart/func"
@@ -12,23 +11,8 @@ func main() {
 	if len(os.Args) > 4 || len(os.Args) == 1 {
 		return
 	}
-	// add the logic for run program in all cases just temp
-	switch len(os.Args) {
-	case 2:
-		Content := asciiart.Splite("standard")
-		asciiart.PrintSymbole(Content, os.Args[1])
-	case 3:
-		Content := asciiart.Splite(os.Args[2])
-		asciiart.PrintSymbole(Content, os.Args[1])
-	case 4:
-		fmt.Println(os.Args[1][0:8])
-		if os.Args[1][0:9] != "--output=" {
-			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]")
-			return
-		}
-		// Split the banner (os.Args[2] == name of banner)
-		Content := asciiart.Splite(os.Args[3])
-		// Print the text of user input  (os.Args[1] == text)
-		asciiart.PrintSymbole(Content, os.Args[2])
-	}
+
+	Content := asciiart.Splite("standard")
+	asciiart.PrintSymbole(Content, os.Args[1])
+
 }
